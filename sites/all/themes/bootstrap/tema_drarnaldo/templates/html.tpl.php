c431b00fe6d7a4a4fd987dc6a266c4b3a7943c44
u436059320 <u436059320@srv16.main-hosting.eu>
2016-01-08 12:27:17 +0000

    teste

---
+++ sites/all/themes/bootstrap/tema_drarnaldo/templates/html.tpl.php
@@ -0,0 +1,76 @@
+﻿<?php
+
+/**
+ * @file
+ * Default theme implementation to display the basic html structure of a single
+ * Drupal page.
+ *
+ * Variables:
+ * - $css: An array of CSS files for the current page.
+ * - $language: (object) The language the site is being displayed in.
+ *   $language->language contains its textual representation.
+ *   $language->dir contains the language direction. It will either be 'ltr' or 'rtl'.
+ * - $rdf_namespaces: All the RDF namespace prefixes used in the HTML document.
+ * - $grddl_profile: A GRDDL profile allowing agents to extract the RDF data.
+ * - $head_title: A modified version of the page title, for use in the TITLE
+ *   tag.
+ * - $head_title_array: (array) An associative array containing the string parts
+ *   that were used to generate the $head_title variable, already prepared to be
+ *   output as TITLE tag. The key/value pairs may contain one or more of the
+ *   following, depending on conditions:
+ *   - title: The title of the current page, if any.
+ *   - name: The name of the site.
+ *   - slogan: The slogan of the site, if any, and if there is no title.
+ * - $head: Markup for the HEAD section (including meta tags, keyword tags, and
+ *   so on).
+ * - $styles: Style tags necessary to import all CSS files for the page.
+ * - $scripts: Script tags necessary to load the JavaScript files and settings
+ *   for the page.
+ * - $page_top: Initial markup from any modules that have altered the
+ *   page. This variable should always be output first, before all other dynamic
+ *   content.
+ * - $page: The rendered page content.
+ * - $page_bottom: Final closing markup from any modules that have altered the
+ *   page. This variable should always be output last, after all other dynamic
+ *   content.
+ * - $classes String of classes that can be used to style contextually through
+ *   CSS.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_html()
+ * @see template_process()
+ *
+ * @ingroup themeable
+ */
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
+  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
+
+<head profile="<?php print $grddl_profile; ?>">
+<meta content="Instituto do Câncer Doutor Arnaldo Vieira de Carvalho" name="O PRIMEIRO HOSPITAL DE CÂNCER DO BRASIL ">
+ <meta name="description" content="O PRIMEIRO HOSPITAL DE CÂNCER DO BRASIL - Os pacientes da rede pública de saúde elegeram o Instituto do Câncer Arnaldo Vieira de Carvalho (ICAVC) como o hospital de melhor internação humanizada de todo o Estado de São Paulo, conveniados ao SUS (Sistema Único de Saúde)....">
+<meta name="keywords" content="Instituto do cancer, Instituto do câncer, hospital do cancer, hospital do câncer, doutor arnaldo, doutor arnaldo vieira de carvalho, Instituto do cancer doutor arnaldo vieira de carvalho, doação instituto do cancer, doação instituição filantrópica, doações para instituições filantrópicas, donate, doar, doação, doações">
+<meta name="author" content="Danilo Rodrigues Bispo">
+<!--COMANDOS PARA LIMPAR O CACHE DO NAVEGADOR TODA VEZ QUE O USUÁRIO ACESSAR O SITE!-->
+<meta http-equiv="cache-control" content="max-age=0" />
+<meta http-equiv="cache-control" content="no-cache" />
+<meta http-equiv="expires" content="0" />
+<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+<meta http-equiv="pragma" content="no-cache" />
+<!--ACABA COMANDO LIMPA CACHE-->
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  
+</script>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
